dcbd0ceddd12fc81ebf8efcfd9a5b679324cf742
AlekseySelivanov <salex92@mail.ru>
2020-09-20 18:11:10 +0300

    Пишем Main

---
+++ src/ru/geekbrains/homework5/Main.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.homework5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog a = new Dog();
+        Cat b = new Cat();
+
+
+        a.run();
+        a.swim();
+        a.jump();
+        b.run();
+        b.jump();
+    }
+}
